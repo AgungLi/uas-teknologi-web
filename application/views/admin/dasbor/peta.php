649c375fae45ea0b669f077b93ac7d314e434dcf
Agung Li <agungismail414@gmail.com>
2020-06-12 17:47:58 +0700

    Membuat peta penyebaran covid19 di indonesia berdasarkan provinsi menggunakan webgis

---
+++ application/views/admin/dasbor/peta.php
@@ -0,0 +1,10 @@
+<div class="row" style="width: 100%;">
+    <div class="col-md-3">
+        <ul class="list-group list-covid">
+
+        </ul>
+    </div>
+    <div class="col-md-9 bg-dark">
+        <div id="map"></div>
+    </div>
+</div>
--- application/views/admin/layout/sidebar.php
+++ application/views/admin/layout/sidebar.php
@@ -5,19 +5,13 @@
         <img src="<?php echo base_url() ?>assets/template/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">UAS TEKWEB</span>
     </a>
+    <a href="<?php echo base_url('admin/dasbor') ?>" class="brand-link">
+        <img src="<?php echo base_url() ?>assets/template/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light"><?php echo $this->session->userdata('nama'); ?></span>
+    </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
-        <!-- Sidebar user (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="<?php echo base_url() ?>assets/template/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block"><?php echo $this->session->userdata('nama'); ?></a>
-            </div>
-        </div>
-
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
@@ -26,6 +20,15 @@
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard Covid 19
+                        </p>
+                    </a>
+                </li>
+
+                <li class="nav-item has-treeview">
+                    <a href="<?php echo base_url('admin/peta') ?>" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Peta Penyebaran Covid 19
                         </p>
                     </a>
                 </li>
